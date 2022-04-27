883bbdd8e91ffb16b82e441a256485adb57c421b
Vic <74506415+victorioxd@users.noreply.github.com>
2022-04-27 18:29:00 +0200

    ✨ Create top songs API

--- pages/api/songs.ts
+++ pages/api/songs.ts
@@ -1,16 +1,24 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
-import { get } from 'http';
+import axios from 'axios'
 type Data = {
-  name: string
+  songs: {}[]
 }
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-    get({
-          
-    })
-  res.status(200).json({ name: 'John Doe' })
+  
+  const rawData = axios({
+    method: 'get',
+    url: 'https://api.spotify.com/v1/playlists/37i9dQZEVXbMDoHDwVN2tF',
+    headers: {
+      Authorization: `Bearer ${process.env.SPOTIFY_TOKEN}`,
+    },
+  })
+
+  rawData.then(data => {
+    res.status(200).json ({songs: data.data.tracks.items})
+  })
 }
